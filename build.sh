6b37a96111f8a63a68ba709ec53e59afa603c1f5
Harold Gao <u7869833@anu.edu.au>
2025-10-02 23:09:12 +1000

    Updated cmake to work on mac

--- build.sh
+++ build.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
+set -e
 
-if [ ! -d "build" ]; then
-    mkdir build
-fi
-rm -rf ./build/*
+ARCH=$(uname -m)
+ARCH_FLAG=""
+
+case "$ARCH" in
+    x86_64)
+        ;;
+    arm64|aarch64)
+        ARCH_FLAG="-DCMAKE_OSX_ARCHITECTURES=arm64"
+        ;;
+    *)
+        ;;
+esac
+
+
+mkdir -p build
+rm -rf build/*
 cd build
-cmake ..
-make
+cmake .. $ARCH_FLAG
+make -j"$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 4)"
 cd ..
+
 ./build/revelation
